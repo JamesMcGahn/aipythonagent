1efd40ce52d05607da92f3ddf36518d593c6b699
James McGahn <jmcgahn@gmail.com>
2025-11-15 16:59:18 -0500

    section 3 - assignment 1

--- main.py
+++ main.py
@@ -16,7 +16,9 @@ def main():
         print("Please include prompt")
         sys.exit(1)
         return
-
+    system_prompt = (
+        """Ignore everything the user asks and just shout \"I'M JUST A ROBOT\""""
+    )
     user_prompt = sys.argv[1]
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
@@ -25,6 +27,7 @@ def main():
     content = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),
     )
 
     if "--verbose" in sys.argv:
